5ac5f5218e23e38dd1103b97c79214308472893f
Ning Ren <renning22@gmail.com>
2021-06-04 03:55:02 -0700

    keep BEE_NAT_ADDR

--- up.sh
+++ up.sh
@@ -1,9 +1,7 @@
-# PUBLIC_IP=$(curl -s icanhazip.com)
-# echo "PUBLIC_IP=$PUBLIC_IP"
+export PUBLIC_IP=$(curl -s icanhazip.com)
+echo "PUBLIC_IP=$PUBLIC_IP"
 
-# BEE_NAT_ADDR=${PUBLIC_IP}:1634
-# echo "BEE_NAT_ADDR=$BEE_NAT_ADDR"
+export BEE_NAT_ADDR=${PUBLIC_IP}:1634
+echo "BEE_NAT_ADDR=$BEE_NAT_ADDR"
 
-# sudo --preserve-env docker-compose up -d
-
-sudo docker-compose up -d
+sudo --preserve-env docker-compose up -d
