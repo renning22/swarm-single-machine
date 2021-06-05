c36ef61310bab994520ddc895d08fcf2d1d8f406
Ning Ren <renning22@gmail.com>
2021-06-05 07:44:18 -0700

    swap_endpoint enable websocket

--- start_eth_swap_endpoint.sh
+++ start_eth_swap_endpoint.sh
@@ -1,3 +1,8 @@
 # curl --data '{"method":"eth_blockNumber","params":[],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST localhost:8545
 
-sudo docker run --detach -p 8545:8545 -p 30303:30303 -v /data/ethereum:/root/.ethereum ethereum/client-go:stable --syncmode "light" --cache=512 --rpc --rpcaddr "0.0.0.0" --goerli
+sudo docker run --detach \
+  -p 8545:8545 \
+  -p 8546:8546 \
+  -p 30303:30303 \
+  -v /data/ethereum:/root/.ethereum \
+  ethereum/client-go:stable --syncmode "light" --cache=512 --rpc --rpcaddr "0.0.0.0" --ws --ws.addr "0.0.0.0" --goerli
