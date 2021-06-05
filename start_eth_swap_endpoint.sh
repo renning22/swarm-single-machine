bc4f7217cd4bebd1248899cc96f005cfbd1c9ffb
Ning Ren <renning22@gmail.com>
2021-06-05 04:57:08 -0700

    start_eth_swap_endpoint.sh

---
+++ start_eth_swap_endpoint.sh
@@ -0,0 +1,3 @@
+# curl --data '{"method":"eth_blockNumber","params":[],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST localhost:8545
+
+sudo docker run --detach -p 8545:8545 -p 30303:30303 -v /data/ethereum:/root/.ethereum ethereum/client-go:stable --syncmode "light" --cache=512 --rpc --rpcaddr "0.0.0.0" --goerli
