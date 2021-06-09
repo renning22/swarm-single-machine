ca0c20faa6a37df977f5ef7006e29198cb11aafd
Ning Ren <renning22@gmail.com>
2021-06-09 01:03:16 -0700

    tyun first 10

--- admin/tyun_deploy_first_10.py
+++ admin/tyun_deploy_first_10.py
@@ -11,11 +11,19 @@ from invoke import UnexpectedExit
 hosts = [
     'BEE-xh001',
     'BEE-xh002',
+    'BEE-xh003',
+    'BEE-xh004',
+    'BEE-xh005',
+    'BEE-xh006',
+    'BEE-xh007',
+    'BEE-xh008',
+    'BEE-xh009',
+    'BEE-xh010',
 ]
 
 for i, host in enumerate(hosts):
     print(f'==== {i:02}/{len(hosts):02} : {host} ====')
-    conn = Connection(f'ubuntu@{host}')
+    conn = Connection(host)
     conn.run('hostname')
     try:
         conn.run('git clone https://github.com/renning22/swarm-single-machine.git && cd swarm-single-machine && ./init.sh')
